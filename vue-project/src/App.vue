80616596aa773b92bd528d3dd1e854e339324f45
Ilya <westandsouth2@gmail.com>
2022-07-19 16:36:33 +0300

    init vue project and start

---
+++ vue-project/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+  <h1>{{msg}}</h1> 
+  <h2>
+   Hello world
+  </h2>
+
+</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+
+h1, h2 {
+  font-weight: normal;
+}
+
+</style>
